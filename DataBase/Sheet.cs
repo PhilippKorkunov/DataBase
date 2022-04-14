272bd5283bb7117af71fea441eb7fe2a00bc38f1
PhilippKorkunov <filippkorkunov.1920@gmail.com>
2022-04-14 17:50:17 +0300

    Добавьте файлы проекта.

---
+++ DataBase/Sheet.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using OfficeOpenXml;
+
+namespace DataBase
+{
+    internal class Sheet
+    {
+        internal static string path = Path.Combine("C:/Users/filip/source/repos/DataBase/DataBase/", "thrlist.xlsx");
+        internal static string link = "https://bdu.fstec.ru/files/documents/thrlist.xlsx";
+        internal static Dictionary<string, Threat> threatDict = new Dictionary<string, Threat>();
+
+        internal static bool Exists() { return File.Exists(path); }
+
+        internal static void Upload()
+        {
+            if (!Exists())
+            {
+                WebClient myWebClient = new WebClient();
+                myWebClient.DownloadFile(link, path);
+            }
+        }
+
+        internal static void Delite()
+        {
+            if (Exists())
+            {
+                File.Delete(path);
+            }
+        }
+
+        internal static void Parse()
+        {
+            FileInfo existingFile = new FileInfo(path);
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            using (ExcelPackage package = new ExcelPackage(existingFile))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+                worksheet.DeleteRow(1);
+                worksheet.DeleteRow(1);
+                int colCount = worksheet.Dimension.End.Column;
+                int rowCount = worksheet.Dimension.End.Row;
+                for (int row = 1; row <= rowCount; row++)
+                {
+                    Threat threat = new Threat();
+
+                    threat.Id = int.Parse(worksheet.Cells[row, 1].Value.ToString().Trim());
+
+                    threat.Name = worksheet.Cells[row, 2].Value.ToString().Trim();
+
+                    threat.Description = worksheet.Cells[row, 3].Value.ToString();
+
+                    threat.Source = worksheet.Cells[row, 4].Value.ToString();
+
+                    threat.Aim = worksheet.Cells[row, 5].Value.ToString();
+
+                    threat.PrivacyBreak = worksheet.Cells[row, 6].Value.ToString();
+
+                    threat.IntegrityBreak = worksheet.Cells[row, 7].Value.ToString();
+
+                    threat.AvailabilityBreak = worksheet.Cells[row, 8].Value.ToString();
+
+                    worksheet.Cells[row, colCount - 1].Style.Numberformat.Format = "dd-MM-yyyy";
+                    DateTime appearDT = DateTime.FromOADate(double.Parse(worksheet.Cells[row, colCount - 1].Value.ToString()));
+                    threat.AppearDate = appearDT;
+
+                    worksheet.Cells[row, colCount].Style.Numberformat.Format = "dd-MM-yyyy";
+                    DateTime updateDT = DateTime.FromOADate(double.Parse(worksheet.Cells[row, colCount].Value.ToString()));
+                    threat.LastUpdateDate = updateDT;
+
+                    threatDict[threat.Name] = threat;
+                }
+            }
+
+        }
+
+        internal static void Update()
+        {
+            Delite();
+            Upload();
+            Parse();
+        }
+
+    }
+}
---
+++ DataBase/Threat.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase
+{
+    internal class Threat
+    {
+        private string privacyBreak = "Нет";
+        private string integrityBreak = "Нет";
+        private string availabilityBreak = "Нет";
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Source { get; set; }
+        public string Aim { get; set; }
+        public string PrivacyBreak
+        {
+            set { privacyBreak = BoolToString(value); }
+            get { return privacyBreak; }
+        }
+        public string IntegrityBreak
+        {
+            set { integrityBreak = BoolToString(value); }
+            get { return integrityBreak; }
+        }
+        public string AvailabilityBreak
+        {
+            set { availabilityBreak = BoolToString(value); }
+            get { return availabilityBreak; }
+        }
+        public DateTime AppearDate { get; set; }
+        public DateTime LastUpdateDate { get; set; }
+
+        private static string BoolToString(string b)
+        {
+            if (b == "1") return "Да";
+            else return "Нет";
+        }
+    }
+}
